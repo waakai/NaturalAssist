faa4bf66014b54dd1bdf6cd8e54ecd065b1bbc26
Victor Wakai <waakai@hotmail.com>
2020-08-30 17:11:43 -0300

    Estrutura inicial do projeto

---
+++ config/env/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: 'development',
+  nomeApi: 'helloworld',
+  urlApi: 'http://localhost',
+  portApi: 3303,
+  dataConfig: {
+    MYSQL: {
+      database: 'cursonode',
+      host: 'localhost',
+      port: '3306',
+      user: 'root',
+      password: '1234'
+    }
+  }
+};
